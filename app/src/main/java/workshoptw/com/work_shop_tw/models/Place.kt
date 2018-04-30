14be743d88c08517af9db1a78ebb82ac361633f8
edgar silva <esilva@thoughtworks.com>
2018-04-30 20:43:29 -0300

    Remove codes to do step 1

--- app/src/main/java/workshoptw/com/work_shop_tw/models/Place.kt
+++
@@ -1,5 +0,0 @@
-package workshoptw.com.work_shop_tw.models
-
-import java.io.Serializable
-
-data class Place(var name: String, var description: String): Serializable
--- app/src/main/java/workshoptw/com/work_shop_tw/views/DetailsActivity.kt
+++ app/src/main/java/workshoptw/com/work_shop_tw/views/DetailsActivity.kt
@@ -1,29 +1,13 @@
 package workshoptw.com.work_shop_tw.views
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_details.*
 import workshoptw.com.work_shop_tw.R
-import workshoptw.com.work_shop_tw.models.Place
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
-
-        buttonDone.setOnClickListener {
-            val name = textName.text.toString()
-            val description = textDescription.text.toString()
-            val place = Place(name, description)
-            val intent = Intent()
-
-            intent.putExtra("place", place)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-        }
     }
 }
