013bac1c005558c1f9f90d53de3431eb86c3a2c2
edgar silva <esilva@thoughtworks.com>
2018-07-01 19:38:22 -0300

    Unit test PlaceViewModel

---
+++ app/src/test/java/workshoptw/com/work_shop_tw/util/RxSchedulersSetup.kt
@@ -0,0 +1,37 @@
+package workshoptw.com.work_shop_tw.util
+
+
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.annotations.NonNull
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+object RxSchedulerTestSetup {
+
+    fun setupRxScheduler() {
+        val immediate = object : Scheduler() {
+            override fun scheduleDirect(@NonNull run: Runnable, delay: Long, @NonNull unit: TimeUnit): Disposable {
+                return super.scheduleDirect(run, 0, unit)
+            }
+
+            override fun createWorker(): Scheduler.Worker {
+                return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+            }
+        }
+
+        RxJavaPlugins.setInitIoSchedulerHandler { immediate }
+        RxJavaPlugins.setInitComputationSchedulerHandler { immediate }
+        RxJavaPlugins.setInitNewThreadSchedulerHandler { immediate }
+        RxJavaPlugins.setInitSingleSchedulerHandler { immediate }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediate }
+    }
+
+    fun reset() {
+        RxAndroidPlugins.reset()
+        RxJavaPlugins.reset()
+    }
+}
---
+++ app/src/test/java/workshoptw/com/work_shop_tw/views/place/PlaceViewModelTest.kt
@@ -0,0 +1,67 @@
+package workshoptw.com.work_shop_tw.views.place
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Flowable
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import workshoptw.com.work_shop_tw.models.place.Place
+import workshoptw.com.work_shop_tw.models.place.PlaceDAO
+import workshoptw.com.work_shop_tw.util.RxSchedulerTestSetup
+
+@RunWith(MockitoJUnitRunner::class)
+class PlaceViewModelTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var dao: PlaceDAO
+
+    private lateinit var viewModel: PlaceViewModel
+
+    @Before
+    fun setUp() {
+        RxSchedulerTestSetup.setupRxScheduler()
+        viewModel = PlaceViewModel(dao)
+    }
+
+    @After
+    fun tearDown() {
+        RxSchedulerTestSetup.reset()
+    }
+
+    @Test
+    fun completeWhenSavePlace() {
+        viewModel.savePlace(Place("", ""))
+                .test()
+                .assertNoValues()
+                .assertComplete()
+    }
+
+    @Test
+    fun returnsAllSavedPlacesWhenGetAll() {
+        whenever(dao.getAll()).thenReturn(Flowable.just(getPlaces()))
+
+        viewModel.getAll().test()
+                .assertValue {
+                    it.size == 2
+                }.assertComplete()
+    }
+
+    @Test
+    fun emptyWhenAreNoItemsSaved() {
+        whenever(dao.getAll()).thenReturn(Flowable.empty())
+
+        viewModel.getAll().test()
+                .assertNoValues()
+                .assertComplete()
+    }
+
+    private fun getPlaces(): List<Place> =
+            listOf(Place("", ""), Place("", ""))
+}
